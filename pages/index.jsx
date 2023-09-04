c0e5bea66b7c35f6f7bd0fa539a2360c7ec23416
if <trident.ito@gmail.com>
2023-09-05 04:20:43 +0900

    変更

--- pages/index.jsx
+++ pages/index.jsx
@@ -120,8 +120,6 @@ export default function Home() {
               <p>本文</p>
               <p>本文</p>
               <p>本文</p>
-              <p>本文</p>
-              <p>本文</p>
             </section>
 
 
